a6198cca9a349416244a85dd01c24e7c60424d72
Donghyeok Tak <tdh8316@naver.com>
2024-06-19 15:35:07 +0900

    Add a search page

--- CourseRegistration/SearchPage.Designer.cs
+++ CourseRegistration/SearchPage.Designer.cs
@@ -28,28 +28,375 @@
         /// </summary>
         private void InitializeComponent()
         {
+            columnHeader1 = new ColumnHeader();
             label1 = new Label();
+            SearchText = new TextBox();
+            SearchBtn = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            groupBox1 = new GroupBox();
+            SearchListView = new ListView();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            columnHeader6 = new ColumnHeader();
+            columnHeader7 = new ColumnHeader();
+            groupBox2 = new GroupBox();
+            splitContainer1 = new SplitContainer();
+            label4 = new Label();
+            label9 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            GradingLabel = new Label();
+            TeamProjLabel = new Label();
+            ExamLabel = new Label();
+            AttendanceLabel = new Label();
+            AssignmentLabel = new Label();
+            RatingLabel = new Label();
+            groupBox3 = new GroupBox();
+            commentListView = new ListView();
+            columnHeader8 = new ColumnHeader();
+            columnHeader9 = new ColumnHeader();
+            columnHeader10 = new ColumnHeader();
+            SearchCmb = new ComboBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            groupBox3.SuspendLayout();
             SuspendLayout();
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "과목명";
+            columnHeader1.Width = 160;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("맑은 고딕", 36F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            label1.Location = new Point(511, 424);
+            label1.Font = new Font("맑은 고딕", 28F, FontStyle.Bold);
+            label1.Location = new Point(55, 56);
             label1.Name = "label1";
-            label1.Size = new Size(297, 65);
-            label1.TabIndex = 1;
-            label1.Text = "Search Page";
+            label1.Size = new Size(299, 51);
+            label1.TabIndex = 0;
+            label1.Text = "Search Courses";
+            // 
+            // SearchText
+            // 
+            SearchText.Location = new Point(55, 135);
+            SearchText.Name = "SearchText";
+            SearchText.Size = new Size(422, 23);
+            SearchText.TabIndex = 1;
+            SearchText.KeyUp += SearchText_KeyUp;
+            // 
+            // SearchBtn
+            // 
+            SearchBtn.Location = new Point(610, 134);
+            SearchBtn.Name = "SearchBtn";
+            SearchBtn.Size = new Size(75, 27);
+            SearchBtn.TabIndex = 2;
+            SearchBtn.Text = "Search";
+            SearchBtn.UseVisualStyleBackColor = true;
+            SearchBtn.Click += SearchBtn_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 28F, FontStyle.Bold);
+            label2.Location = new Point(706, 110);
+            label2.Name = "label2";
+            label2.Size = new Size(283, 51);
+            label2.TabIndex = 3;
+            label2.Text = "Course Details";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("맑은 고딕", 28F, FontStyle.Bold);
+            label3.Location = new Point(706, 463);
+            label3.Name = "label3";
+            label3.Size = new Size(355, 51);
+            label3.TabIndex = 4;
+            label3.Text = "Course Comments";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(SearchListView);
+            groupBox1.Location = new Point(55, 164);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(630, 710);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            // 
+            // SearchListView
+            // 
+            SearchListView.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5, columnHeader6, columnHeader7 });
+            SearchListView.Dock = DockStyle.Fill;
+            SearchListView.FullRowSelect = true;
+            SearchListView.GridLines = true;
+            SearchListView.HeaderStyle = ColumnHeaderStyle.Nonclickable;
+            SearchListView.Location = new Point(3, 19);
+            SearchListView.MultiSelect = false;
+            SearchListView.Name = "SearchListView";
+            SearchListView.Size = new Size(624, 688);
+            SearchListView.TabIndex = 0;
+            SearchListView.UseCompatibleStateImageBehavior = false;
+            SearchListView.View = View.Details;
+            SearchListView.SelectedIndexChanged += SearchListView_SelectedIndexChanged;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "담당교수";
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "시간";
+            columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "과목코드";
+            columnHeader4.Width = 120;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "강의실";
+            // 
+            // columnHeader6
+            // 
+            columnHeader6.Text = "학점";
+            // 
+            // columnHeader7
+            // 
+            columnHeader7.Text = "평점";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(splitContainer1);
+            groupBox2.Location = new Point(721, 164);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(536, 254);
+            groupBox2.TabIndex = 6;
+            groupBox2.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(3, 19);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(label4);
+            splitContainer1.Panel1.Controls.Add(label9);
+            splitContainer1.Panel1.Controls.Add(label5);
+            splitContainer1.Panel1.Controls.Add(label6);
+            splitContainer1.Panel1.Controls.Add(label8);
+            splitContainer1.Panel1.Controls.Add(label7);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.BackColor = SystemColors.Window;
+            splitContainer1.Panel2.Controls.Add(GradingLabel);
+            splitContainer1.Panel2.Controls.Add(TeamProjLabel);
+            splitContainer1.Panel2.Controls.Add(ExamLabel);
+            splitContainer1.Panel2.Controls.Add(AttendanceLabel);
+            splitContainer1.Panel2.Controls.Add(AssignmentLabel);
+            splitContainer1.Panel2.Controls.Add(RatingLabel);
+            splitContainer1.Size = new Size(530, 232);
+            splitContainer1.SplitterDistance = 218;
+            splitContainer1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
+            label4.Location = new Point(12, 10);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 21);
+            label4.TabIndex = 0;
+            label4.Text = "Ratings";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
+            label9.Location = new Point(12, 195);
+            label9.Name = "label9";
+            label9.Size = new Size(71, 21);
+            label9.TabIndex = 5;
+            label9.Text = "Grading";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
+            label5.Location = new Point(11, 45);
+            label5.Name = "label5";
+            label5.Size = new Size(99, 21);
+            label5.TabIndex = 1;
+            label5.Text = "Assignment";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
+            label6.Location = new Point(12, 82);
+            label6.Name = "label6";
+            label6.Size = new Size(98, 21);
+            label6.TabIndex = 2;
+            label6.Text = "Attendance";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
+            label8.Location = new Point(12, 158);
+            label8.Name = "label8";
+            label8.Size = new Size(111, 21);
+            label8.TabIndex = 4;
+            label8.Text = "Team Project";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("맑은 고딕", 12F, FontStyle.Bold);
+            label7.Location = new Point(11, 119);
+            label7.Name = "label7";
+            label7.Size = new Size(52, 21);
+            label7.TabIndex = 3;
+            label7.Text = "Exam";
+            // 
+            // GradingLabel
+            // 
+            GradingLabel.AutoSize = true;
+            GradingLabel.Location = new Point(18, 201);
+            GradingLabel.Name = "GradingLabel";
+            GradingLabel.Size = new Size(12, 15);
+            GradingLabel.TabIndex = 5;
+            GradingLabel.Text = "-";
+            // 
+            // TeamProjLabel
+            // 
+            TeamProjLabel.AutoSize = true;
+            TeamProjLabel.Location = new Point(18, 164);
+            TeamProjLabel.Name = "TeamProjLabel";
+            TeamProjLabel.Size = new Size(12, 15);
+            TeamProjLabel.TabIndex = 4;
+            TeamProjLabel.Text = "-";
+            // 
+            // ExamLabel
+            // 
+            ExamLabel.AutoSize = true;
+            ExamLabel.Location = new Point(18, 125);
+            ExamLabel.Name = "ExamLabel";
+            ExamLabel.Size = new Size(12, 15);
+            ExamLabel.TabIndex = 3;
+            ExamLabel.Text = "-";
+            // 
+            // AttendanceLabel
+            // 
+            AttendanceLabel.AutoSize = true;
+            AttendanceLabel.Location = new Point(18, 88);
+            AttendanceLabel.Name = "AttendanceLabel";
+            AttendanceLabel.Size = new Size(12, 15);
+            AttendanceLabel.TabIndex = 2;
+            AttendanceLabel.Text = "-";
+            // 
+            // AssignmentLabel
+            // 
+            AssignmentLabel.AutoSize = true;
+            AssignmentLabel.Location = new Point(18, 51);
+            AssignmentLabel.Name = "AssignmentLabel";
+            AssignmentLabel.Size = new Size(12, 15);
+            AssignmentLabel.TabIndex = 1;
+            AssignmentLabel.Text = "-";
+            // 
+            // RatingLabel
+            // 
+            RatingLabel.AutoSize = true;
+            RatingLabel.Location = new Point(18, 15);
+            RatingLabel.Name = "RatingLabel";
+            RatingLabel.Size = new Size(12, 15);
+            RatingLabel.TabIndex = 0;
+            RatingLabel.Text = "-";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(commentListView);
+            groupBox3.Location = new Point(721, 517);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(536, 354);
+            groupBox3.TabIndex = 7;
+            groupBox3.TabStop = false;
+            // 
+            // commentListView
+            // 
+            commentListView.Columns.AddRange(new ColumnHeader[] { columnHeader8, columnHeader9, columnHeader10 });
+            commentListView.GridLines = true;
+            commentListView.Location = new Point(15, 19);
+            commentListView.MultiSelect = false;
+            commentListView.Name = "commentListView";
+            commentListView.Size = new Size(518, 332);
+            commentListView.TabIndex = 0;
+            commentListView.UseCompatibleStateImageBehavior = false;
+            commentListView.View = View.Details;
+            // 
+            // columnHeader8
+            // 
+            columnHeader8.Text = "수강기간";
+            columnHeader8.Width = 120;
+            // 
+            // columnHeader9
+            // 
+            columnHeader9.Text = "평점";
+            // 
+            // columnHeader10
+            // 
+            columnHeader10.Text = "내용";
+            columnHeader10.Width = 360;
+            // 
+            // SearchCmb
+            // 
+            SearchCmb.DropDownStyle = ComboBoxStyle.DropDownList;
+            SearchCmb.FormattingEnabled = true;
+            SearchCmb.Items.AddRange(new object[] { "과목명", "담당교수" });
+            SearchCmb.Location = new Point(483, 135);
+            SearchCmb.Name = "SearchCmb";
+            SearchCmb.Size = new Size(121, 23);
+            SearchCmb.TabIndex = 8;
             // 
             // SearchPage
             // 
             AutoScaleMode = AutoScaleMode.None;
             BackColor = Color.White;
             ClientSize = new Size(1299, 951);
+            Controls.Add(SearchCmb);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(SearchBtn);
+            Controls.Add(SearchText);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "SearchPage";
             Text = "Registration";
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -57,5 +404,38 @@
         #endregion
 
         private Label label1;
+        private TextBox SearchText;
+        private Button SearchBtn;
+        private Label label2;
+        private Label label3;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private ListView SearchListView;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+        private ColumnHeader columnHeader7;
+        private ComboBox SearchCmb;
+        private Label label5;
+        private Label label4;
+        private Label label7;
+        private Label label6;
+        private SplitContainer splitContainer1;
+        private Label label8;
+        private Label label9;
+        private Label GradingLabel;
+        private Label TeamProjLabel;
+        private Label ExamLabel;
+        private Label AttendanceLabel;
+        private Label AssignmentLabel;
+        private Label RatingLabel;
+        private ListView commentListView;
+        private ColumnHeader columnHeader8;
+        private ColumnHeader columnHeader9;
+        private ColumnHeader columnHeader10;
     }
 }
--- CourseRegistration/SearchPage.cs
+++ CourseRegistration/SearchPage.cs
@@ -1,25 +1,293 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+
 
 namespace CourseRegistration
 {
     public partial class SearchPage : Form
     {
+        List<Root>? roots = null;
+
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+       (
+           int nLeftRect,     // x-coordinate of upper-left corner
+           int nTopRect,      // y-coordinate of upper-left corner
+           int nRightRect,    // x-coordinate of lower-right corner
+           int nBottomRect,   // y-coordinate of lower-right corner
+           int nWidthEllipse, // width of ellipse
+           int nHeightEllipse // height of ellipse
+       );
         public SearchPage()
         {
             InitializeComponent();
+            SearchCmb.SelectedIndex = 0;
+
+            SearchListView.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, SearchListView.Width, SearchListView.Height, 16, 16));
+
+            LoadCourseData();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        void LoadCourseData()
         {
+            string jsonString = File.ReadAllText("everytime-lectures.json");
+            roots = JsonSerializer.Deserialize<List<Root>>(jsonString);
+            Random rnd = new();
 
+            string[] times = ["월4수3", "화2목3", "수6", "목5", "금123"];
+            if (roots != null)
+            {
+                foreach (Root root in roots)
+                {
+                    ListViewItem item = SearchListView.Items.Add(root.name);
+                    item.SubItems.Add(root.professor);
+                    item.SubItems.Add(times[rnd.Next(times.Length)]);
+                    item.SubItems.Add("0000-000-0000");
+                    item.SubItems.Add("-"); // Room
+                    item.SubItems.Add("3"); // Credit
+                    item.SubItems.Add(root.rate?.average.ToString());
+                }
+            }
         }
+
+        private void SearchListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (SearchListView.SelectedIndices.Count == 0) return;
+
+            int index = SearchListView.SelectedIndices[0];
+            if (index != -1)
+            {
+                _UpdateDetailsView(index);
+                _UpdateCommentsView(index);
+            }
+        }
+
+        private void _UpdateDetailsView(int index)
+        {
+            Root data = roots![index];
+            RatingLabel.Text = data.rate?.average.ToString();
+
+            int i = 0;
+            int max = -1;
+            int maxIndex = -1;
+
+            Detail? assignment = null;
+            while (assignment == null)
+            {
+                if (data.details![i].name == "과제")
+                    assignment = data.details![i];
+                i++;
+            }
+            for (i = 0; i < assignment!.items!.Count; i++)
+            {
+                Item assignItem = assignment!.items![i];
+                if (assignItem.count > max)
+                {
+                    max = assignItem.count;
+                    maxIndex = i;
+                }
+            }
+            AssignmentLabel.Text = assignment!.items![maxIndex].text;
+
+            i = 0;
+            max = -1;
+            maxIndex = -1;
+            Detail? attendance = null;
+            while (attendance == null)
+            {
+                if (data.details![i].name == "출결")
+                    attendance = data.details![i];
+                i++;
+            }
+            for (i = 0; i < attendance!.items!.Count; i++)
+            {
+                Item attendItem = attendance!.items![i];
+                if (attendItem.count > max)
+                {
+                    max = attendItem.count;
+                    maxIndex = i;
+                }
+            }
+            AttendanceLabel.Text = attendance!.items![maxIndex].text;
+
+            i = 0;
+            max = -1;
+            maxIndex = -1;
+            Detail? exam = null;
+            while (exam == null)
+            {
+                if (data.details![i].name == "시험")
+                    exam = data.details![i];
+                i++;
+            }
+            for (i = 0; i < exam!.items!.Count; i++)
+            {
+                Item examItem = exam!.items![i];
+                if (examItem.count > max)
+                {
+                    max = examItem.count;
+                    maxIndex = i;
+                }
+            }
+            ExamLabel.Text = exam!.items![maxIndex].text;
+
+            i = 0;
+            max = -1;
+            maxIndex = -1;
+            Detail? team = null;
+            while (team == null)
+            {
+                if (data.details![i].name == "조모임")
+                    team = data.details![i];
+                i++;
+            }
+            for (i = 0; i < team!.items!.Count; i++)
+            {
+                Item teamItem = team!.items![i];
+                if (teamItem.count > max)
+                {
+                    max = teamItem.count;
+                    maxIndex = i;
+                }
+            }
+            TeamProjLabel.Text = team!.items![maxIndex].text;
+
+            i = 0;
+            max = -1;
+            maxIndex = -1;
+            Detail? grading = null;
+            while (grading == null)
+            {
+                if (data.details![i].name == "성적")
+                    grading = data.details![i];
+                i++;
+            }
+            for (i = 0; i < grading!.items!.Count; i++)
+            {
+                Item gradingItem = grading!.items![i];
+                if (gradingItem.count > max)
+                {
+                    max = gradingItem.count;
+                    maxIndex = i;
+                }
+            }
+            GradingLabel.Text = grading!.items![maxIndex].text;
+        }
+
+        private void _UpdateCommentsView(int index)
+        {
+            commentListView.Items.Clear();
+
+            Root data = roots![index];
+
+            string[] fakeDates = ["19년 1학기", "19년 2학기", "18년 1학기", "18년 2학기",
+                "22년 1학기", "22년 2학기", "23년 1학기", "23년 2학기", "24년 1학기"];
+            Random rnd = new();
+
+            ListViewItem item;
+            for (int i = 0; i < 32; ++i)
+            {
+                item = commentListView.Items.Add(fakeDates[rnd.Next(fakeDates.Length)]);
+                item.SubItems.Add(" - "); // Rating
+                item.SubItems.Add("This is a comment #" + i + " for " + data.name + "(" + data.professor + ")");
+            }
+        }
+
+        private void Search()
+        {
+            SearchListView.Items.Clear();
+
+            string jsonString = File.ReadAllText("everytime-lectures.json");
+            string query = SearchText.Text;
+            List<Root>? tmpRoots = JsonSerializer.Deserialize<List<Root>>(jsonString);
+            roots!.Clear();
+
+            if (SearchCmb.Text == "과목명")
+            {
+                for (int i = 0; i < tmpRoots!.Count; ++i)
+                {
+                    Root root = tmpRoots![i];
+                    if (root.name!.Contains(query))
+                        roots.Add(root);
+                }
+            }
+            else if (SearchCmb.Text == "담당교수")
+            {
+                for (int i = 0; i < tmpRoots!.Count; ++i)
+                {
+                    Root root = tmpRoots![i];
+                    if (root.professor!.Contains(query))
+                        roots.Add(root);
+                }
+            }
+
+            Random rnd = new();
+
+            string[] times = ["월4수3", "화2목3", "수6", "목5", "금123"];
+            if (roots != null)
+            {
+                if (roots.Count == 0)
+                    MessageBox.Show("No matched data!");
+                foreach (Root root in roots)
+                {
+                    ListViewItem item = SearchListView.Items.Add(root.name);
+                    item.SubItems.Add(root.professor);
+                    item.SubItems.Add(times[rnd.Next(times.Length)]);
+                    item.SubItems.Add("0000-000-0000");
+                    item.SubItems.Add("-"); // Room
+                    item.SubItems.Add("3"); // Credit
+                    item.SubItems.Add(root.rate?.average.ToString());
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Data is null!");
+            }
+        }
+
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            Search();
+        }
+
+        private void SearchText_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Search();
+            }
+        }
+    }
+
+    public class Detail
+    {
+        public string? name { get; set; }
+        public int count { get; set; }
+        public List<Item>? items { get; set; }
+    }
+
+    public class Item
+    {
+        public int value { get; set; }
+        public int count { get; set; }
+        public string? text { get; set; }
+    }
+
+    public class Rate
+    {
+        public double average { get; set; }
+        public int count { get; set; }
+        public List<Item>? items { get; set; }
+    }
+
+    public class Root
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? professor { get; set; }
+        public Rate? rate { get; set; }
+        public List<Detail>? details { get; set; }
     }
 }
